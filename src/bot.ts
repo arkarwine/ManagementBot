97fd0172d9af9bc72e810375c4d05d4c03c7524b
arkarwine <arkarwine13579@gmail.com>
2024-02-09 23:31:01 +0630

    deploy again

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,6 @@ import { Bot, webhookCallback } from 'grammy';
 const bot = new Bot('6741337977:AAF1_ah4m5u3lDhUn-SVkBevNOua84EiU6A');
 
 bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
-bot.on('message', (ctx) => ctx.reply('Got another message!'));
+bot.on('message', (ctx) => ctx.reply('Got yet another message!'));
 
 addEventListener('fetch', webhookCallback(bot, 'cloudflare'));
