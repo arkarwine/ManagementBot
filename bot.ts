b40f1c6c7195654870e9f0123d2b7070ddadc2f4
arkarwine <arkarwine13579@gmail.com>
2024-02-09 22:26:07 +0630

    initial commit

---
+++ bot.ts
@@ -0,0 +1,8 @@
+import { Bot } from "grammy";
+
+const bot = new Bot("6741337977:AAF1_ah4m5u3lDhUn-SVkBevNOua84EiU6A");
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+bot.start();
