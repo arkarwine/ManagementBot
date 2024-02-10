a8af1f865273eb46f78ba430533ca0092503b950
arkarwine <arkarwine13579@gmail.com>
2024-02-10 16:51:53 +0630

    fix

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,14 @@
-import { Composer, InputFile } from 'grammy';
-import ytdl from 'ytdl-core';
+import { Composer, InputFile } from "grammy";
+import ytdl from "ytdl-core";
 
 export const arkary = new Composer();
 
-arkary.on('message', async (ctx) => {
-	const audio = await ytdl('https://www.youtube.com/watch?v=0Iy9bahS71s', { quality: 'highestaudio', filter: 'audioonly' });
-	ctx.replyWithAudio(new InputFile(audio));
+arkary.on("message", async (ctx) => {
+    const wait = await ctx.reply("Loading ...");
+    const audio = await ytdl("https://www.youtube.com/watch?v=0Iy9bahS71s", {
+        quality: "highestaudio",
+        filter: "audioonly",
+    });
+    ctx.replyWithAudio(new InputFile(audio));
+    await ctx.api.deleteMessage(ctx.chat.id, wait.message_id);
 });
