ce98e5caa8251796bef6b8be0f9817b387faa8cb
arkarwine <arkarwine13579@gmail.com>
2024-02-10 16:48:16 +0630

    serverless

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-import { Bot } from "grammy";
+import express from "express";
+import { Bot, webhookCallback } from "grammy";
 import { arkary } from "./bot";
 
 const bot = new Bot(process.env.BOT_TOKEN!);
 bot.use(arkary);
 
-bot.start();
+bot.command("start", (ctx) => ctx.reply("Hello World!"));
+
+if (process.env.NODE_ENV === "DEVELOPMENT") {
+    bot.start();
+} else {
+    const port = process.env.PORT || 3000;
+    const app = express();
+    app.use(express.json());
+    app.use(`/${bot.token}`, webhookCallback(bot, "express"));
+    app.listen(port, () => console.log(`listening on port ${port}`));
+}
