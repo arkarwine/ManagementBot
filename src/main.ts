85596d26d4ab01e4279952eb16dbf2f7c6c4f22f
arkarwine <arkarwine13579@gmail.com>
2024-02-10 17:14:45 +0630

    ordinary deploy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
-import express from "express";
-import { Bot, webhookCallback } from "grammy";
+import { Bot } from "grammy";
 import { arkary } from "./bot";
 
 const bot = new Bot(process.env.BOT_TOKEN!);
 bot.use(arkary);
 
-bot.command("start", (ctx) => ctx.reply("Hello World!"));
-
-const server = express();
-const port = process.env.PORT || 8080;
-
-server.use(webhookCallback(bot, "express"));
-server.listen(port, () => console.log(`listening on port ${port}`));
+bot.start();
