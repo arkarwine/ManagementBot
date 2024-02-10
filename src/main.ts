67ba3d85d9d0517076969d3ab826a1eb3b9b0dbd
arkarwine <arkarwine13579@gmail.com>
2024-02-10 21:22:49 +0630

    serverless test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
-import { Bot } from "grammy";
-import { arkary } from "./bot";
+// import { Bot } from "grammy";
+// import { arkary } from "./bot";
 
-const bot = new Bot(process.env.BOT_TOKEN!);
-bot.use(arkary);
+// const bot = new Bot(process.env.BOT_TOKEN!);
+// bot.use(arkary);
 
-bot.start();
+// bot.start();
+
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
