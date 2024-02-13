5cfc9b34a8547b7aff0bc358df78d028cee611c7
arkarwine <arkarwine13579@gmail.com>
2024-02-13 13:09:16 +0630

    support for ytdl vercel api

--- src/bot.ts
+++ src/bot.ts
@@ -1,22 +1,27 @@
 import { Composer, InputFile } from "https://deno.land/x/grammy@v1.20.3/mod.ts";
 
-import PipedAPI from "./PipedApi/index.ts";
+import { videoInfo } from "./types.ts";
 
 const YOUTUBE_URL_REGEX =
     /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]+).*/;
 
+const BASE_URL = "https://ytdl-6dsnjfw0t-behs1.vercel.app/";
+const STREAMING_URL = BASE_URL + "api/download?v=";
 export const arkary = new Composer();
-const piped = new PipedAPI();
 
 arkary.on("message:text", async (ctx) => {
     const video_id = ctx.message.text.match(YOUTUBE_URL_REGEX);
 
     if (video_id) {
         const wait = await ctx.reply("Loading ...");
+        const stream_url = STREAMING_URL + video_id[1];
+        const res = await fetch(stream_url);
 
-        const url = (await piped.streams(video_id[1])).audioStreams.sort(
-            (a, b) => b.bitrate - a.bitrate
-        )[0].url;
+        const data: videoInfo = await res.json();
+
+        const url = data.formats
+            .filter((value) => value.hasAudio && !value.hasVideo)
+            .toSorted((a, b) => b.bitrate! - a.bitrate!)[0].url;
 
         await ctx.api.editMessageText(wait.chat.id, wait.message_id, url);
 
---
+++ src/test.ts
@@ -0,0 +1,16 @@
+import { videoInfo } from "./types.ts";
+
+const BASE_URL = new URL("https://ytdl-6dsnjfw0t-behs1.vercel.app/");
+const STREAMING_URL = new URL("/api/download?v=soP_KKTkSgI", BASE_URL);
+
+const res = await fetch(STREAMING_URL);
+
+console.log(STREAMING_URL.href);
+
+const data: videoInfo = await res.json();
+
+console.log(
+    data.formats
+        .filter((value) => value.hasAudio && !value.hasVideo)
+        .toSorted((a, b) => b.bitrate! - a.bitrate!)[0].url
+);
